f9a615a1e940e0990d42586b33b0f5e05231a9b9
0Lupillo0 <jose.banosm@ingenieria.unam.edu>
2023-12-06 14:43:35 -0600

    Clase Servicio y actualizaciones
    Se crea una clase de tipo servicio que buscará el tipo de servicio en el archivo de precios
    Se crea un objeto de tipo Servicio en la clase Empleado

--- Empleado.java
+++ Empleado.java
@@ -1,5 +1,6 @@
 public class Empleado extends Usuario {
     private String fechaDeNacimiento;
+    private Servicio servicioActual;
 
     public Empleado(String nombre, String apellidoPaterno, String apellidoMaterno, String mail, String numeroCelular, String direccion,String fechaDeNacimiento){
         super(nombre, apellidoPaterno, apellidoMaterno,0, mail, numeroCelular, direccion, null, null);
@@ -13,5 +14,11 @@ public class Empleado extends Usuario {
     }
     public String getFechaDeNacimiento(){
         return fechaDeNacimiento;
+    }
+    public void setServicioActual(Servicio servicioActual){
+        this.servicioActual = servicioActual;
+    }
+    public Servicio getServicioActual(){
+        return servicioActual;
     }
 }
---
+++ Servicio.java
@@ -0,0 +1,16 @@
+public class Servicio {
+    private String tipoDeServicio;
+
+    public Servicio(String tipoDeServicio){
+        this.tipoDeServicio = tipoDeServicio;
+    }
+    public void setTipoDeServicio(String tipoDeServicio){
+        this.tipoDeServicio = tipoDeServicio;
+    }
+    public String getTipoDeServicio(){
+        return tipoDeServicio;
+    }
+
+    //Busca el servicio dentro del archivo de servicios
+
+}
