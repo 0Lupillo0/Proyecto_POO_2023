254f8bce7a20be7894db79b697f06a045048c543
Jhonyfox45 <90577193+Jhonyfox45@users.noreply.github.com>
2023-12-06 22:10:25 -0600

    Comentarios
    se añadieron los comentarios Javadoc a la clase Empleado

--- Empleado.java
+++ Empleado.java
@@ -1,29 +1,86 @@
+/**
+ * Clase que representa a un empleado de nuestro Spa de mascotas.
+ * Hereda de la clase Usuario.
+ *
+ * @author Jhonatan Emmanuel Amaya Esquivel
+ * @version 06/12/2023
+ */
 public class Empleado extends Usuario {
+    /**
+     * Fecha de nacimiento del empleado.
+     */
     private String fechaDeNacimiento;
+    /**
+     * Servicio actual que el empleado está realizando.
+     */
     private Servicio servicioActual;
+    /**
+     * Estado que indica si el empleado está ocupado o no.
+     */
     private boolean ocupado;
-
+    /**
+     * Constructor que inicializa los atributos del empleado con la información proporcionada.
+     *
+     * @param nombre           Nombre del empleado.
+     * @param apellidoPaterno  Apellido paterno del empleado.
+     * @param apellidoMaterno  Apellido materno del empleado.
+     * @param mail             Correo electrónico del empleado.
+     * @param numeroCelular    Número de celular del empleado.
+     * @param direccion        Dirección del empleado.
+     * @param fechaDeNacimiento Fecha de nacimiento del empleado.
+     */
     public Empleado(String nombre, String apellidoPaterno, String apellidoMaterno, String mail, String numeroCelular, String direccion,String fechaDeNacimiento){
         super(nombre, apellidoPaterno, apellidoMaterno,0, mail, numeroCelular, direccion, null, null);
         this.fechaDeNacimiento = fechaDeNacimiento;
         ocupado = false;
     }
+    /**
+     * Constructor vacío de la clase Empleado.
+     */
     public Empleado(){
         super();
     }
+
+    // Métodos de acceso y modificación para los atributos
+    /**
+     * Establece la fecha de nacimiento del empleado.
+     *
+     * @param fechaDeNacimiento Fecha de nacimiento a establecer.
+     */
     public void setFechaDeNacimiento(String fechaDeNacimiento){
         this.fechaDeNacimiento = fechaDeNacimiento;
     }
+    /**
+     * Obtiene la fecha de nacimiento del empleado.
+     *
+     * @return Fecha de nacimiento del empleado.
+     */
     public String getFechaDeNacimiento(){
         return fechaDeNacimiento;
     }
+    /**
+     * Establece el servicio actual que el empleado realizará.
+     *
+     * @param servicioActual Servicio actual a establecer.
+     */
     public void setServicioActual(Servicio servicioActual){
         this.servicioActual = servicioActual;
     }
+    /**
+     * Obtiene el servicio actual que el empleado está realizando.
+     *
+     * @return Servicio actual del empleado.
+     */
     public Servicio getServicioActual(){
         return servicioActual;
     }
 
+    // Métodos específicos de servicios para mascotas
+    /**
+     * Realiza el servicio de bañar a una mascota.
+     *
+     * @param laMascota Mascota a bañar.
+     */
     public void bañarMascota(Mascota laMascota){
         try{
             System.out.println("Estoy bañando a "+laMascota.getNombre());
@@ -38,6 +95,11 @@ public class Empleado extends Usuario {
             System.out.println(e);
         }
     }
+    /**
+     * Realiza el servicio de cortar el pelo a una mascota.
+     *
+     * @param laMascota Mascota a la que se le corta el pelo.
+     */
     public void cortarPeloMascota(Mascota laMascota){
         try{
             System.out.println("Le estoy cortando el pelo a "+laMascota.getNombre());
@@ -53,6 +115,11 @@ public class Empleado extends Usuario {
             System.out.println(e);
         }
     }
+    /**
+     * Realiza el servicio de cortar las uñas a una mascota.
+     *
+     * @param laMascota Mascota a la que se le cortan las uñas.
+     */
     public void cortarUniasMascota(Mascota laMascota){
         try{
             System.out.println("Le estoy cortando las uñas a "+laMascota.getNombre());
@@ -67,6 +134,11 @@ public class Empleado extends Usuario {
             System.out.println(e);
         }
     }
+     /**
+     * Realiza el servicio de cepillar los dientes a una mascota.
+     *
+     * @param laMascota Mascota a la que se le cepillan los dientes.
+     */
     public void cepillarDientesMascota(Mascota laMascota){
         try{
             System.out.println("Le estoy cepillando los dientes a "+laMascota.getNombre());
@@ -81,9 +153,18 @@ public class Empleado extends Usuario {
             System.out.println(e);
         }
     }
+     /**
+     * Obtiene el estado de ocupación del empleado.
+     *
+     * @return `true` si el empleado está ocupado, `false` si no lo está.
+     */
     public boolean getOcupado(){
         return ocupado;
     }
+    /**
+     * Realiza el servicio actual asignado al empleado.
+     * Este método verifica el tipo de servicio y llama al método específico correspondiente.
+     */
     public void realizarServicio(){
         if(servicioActual == null){
             System.out.println("El empleado no tiene ningun servicio pendiente");
